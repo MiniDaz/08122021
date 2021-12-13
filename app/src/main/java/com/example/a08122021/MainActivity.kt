acd4de8de7e21af73e6f42e4cda8c24179c2b54a
MiniDaz <85314363+MiniDaz@users.noreply.github.com>
2021-12-13 14:59:50 +0300

    material tool

--- app/src/main/java/com/example/a08122021/MainActivity.kt
+++ app/src/main/java/com/example/a08122021/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.a08122021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initNavigation()
+
+
+
+
+    }
+    private fun initNavigation(){
+        topAppBar.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.settings-> {
+                   Toast.makeText(this,R.string.srttings,Toast.LENGTH_SHORT).show()
+                 true}
+                else-> false
+            }
+        }
+        bottom_navigation.setOnNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
+
+
+
